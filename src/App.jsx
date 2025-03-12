ae954a2b222e06a5cffe6e269691a2accedbc676
lashasaata <lashasaata@gmail.com>
2025-03-12 01:46:22 -0700

    feat: add Header compenent with base structure and its content

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/header";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+  return (
+    <header>
+      <aside>
+        <span>Momentum</span>
+        <img src="./Hourglass.png" alt="sand timer" />
+      </aside>
+      <nav>
+        <button>თანამშრომლის შექმნა</button>
+        <button>
+          <img src="./add.png" alt="add" />
+          <span>შექმენი ახალი დავალება</span>
+        </button>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
