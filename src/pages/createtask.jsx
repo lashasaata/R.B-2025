46361f9370214c0ae76559ce0a699d7226cefa4a
lashasaata <lashasaata@gmail.com>
2025-03-14 01:40:04 -0700

    refactor: complete title and description validations

--- src/pages/createtask.jsx
+++ src/pages/createtask.jsx
@@ -1,10 +1,53 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import axios from "axios";
+import { Description } from "@headlessui/react";
+
+const schema = yup.object().shape({
+  name: yup
+    .string()
+    .transform((value) => value.trim())
+    .required("სათაური სავალდებულოა")
+    .min(3, "სულ მცირე 3 სიმბოლო")
+    .max(255, "მაქსიმუმ 255 სიმბოლო"),
+  description: yup
+    .string()
+    .transform((value) => (value.trim() === "" ? undefined : value.trim()))
+    .notRequired()
+    .test("minWords", "უნდა შეიცავდეს მინიმუმ 4 სიტყვას", (value) => {
+      if (!value) return true; // Allow empty value
+      return value.split(/\s+/).filter(Boolean).length >= 4;
+    })
+    .max(255, "მაქსიმუმ 255 სიმბოლო"),
+});
+
 function CreateTask() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const inputs = watch();
+  console.log(inputs, errors);
+
+  const makeSubmit = () => {};
   return (
     <main className="flex flex-col gap-[25px]">
       <h1 className="text-[34px] text-[#212529] font-[600] leading-[41px]">
         შექმენი ახალი დავალება
       </h1>
-      <form className="form1 flex flex-col items-end gap-[155px] pt-[65px] pr-[368px] pb-[216px] pl-[55px] mb-[100px] rounded-[4px] border border-solid border-[#ddd2ff]">
+      <form
+        onSubmit={handleSubmit(makeSubmit)}
+        className="form1 flex flex-col items-end gap-[155px] pt-[65px] pr-[368px] pb-[216px] pl-[55px] mb-[100px] rounded-[4px] border border-solid border-[#ddd2ff]"
+      >
         <section className="flex gap-[155px]">
           <section className="flex flex-col gap-[57px]">
             <div className="flex flex-col gap-1">
@@ -12,13 +55,14 @@ function CreateTask() {
                 სათაური*
               </label>
               <input
+                {...register("name")}
                 type="text"
-                name="title"
+                name="name"
                 id="title"
                 className="forminputs h-[45px] w-[550px]"
               />
               <p className="flex flex-col gap-[2px]">
-                <span className="formspans">მინიმუმ 2 სიმბოლო</span>
+                <span className="formspans">მინიმუმ 3 სიმბოლო</span>
                 <span className="formspans">მაქსიმუმ 255 სიმბოლო</span>
               </p>
             </div>
@@ -30,12 +74,13 @@ function CreateTask() {
                 აღწერა
               </label>
               <textarea
+                {...register("description")}
                 name="description"
                 id="description"
                 className="forminputs h-[133px] w-[550px] resize-none text-sm text-[#0D0F10]"
               ></textarea>
               <p className="flex flex-col gap-[2px]">
-                <span className="formspans">მინიმუმ 2 სიმბოლო</span>
+                <span className="formspans">მინიმუმ 4 სიტყვა</span>
                 <span className="formspans">მაქსიმუმ 255 სიმბოლო</span>
               </p>
             </div>
